4faa6c89cce01c62130ef5a6d5fa0fff833da371
James Shubin <james@shubin.ca>
2014-08-28 01:08:03 -0400

    Initial commit of vagrant-puppet-docker-template...
    This is an attempt to prototype a default environment for
    vagrant+puppet+docker hacking. More improvements are needed for it to be
    useful, but it's probably already useful as a reference for now.

---
+++ vagrant/puppet/manifests/site.pp
@@ -0,0 +1,40 @@
+node motd {
+	# this will get put on every host...
+	$url = 'https://ttboj.wordpress.com/'
+	file { '/etc/motd':
+		content => "This is Vagrant+Puppet+Docker! (${url})\n",
+	}
+}
+
+# puppetmaster
+node puppet inherits motd {
+
+	class { '::puppet::server':
+		pluginsync => true,	# do we want to enable pluginsync?
+		storeconfigs => true,	# do we want to enable storeconfigs?
+		autosign => [
+			'*',		# FIXME: this is a temporary solution
+			#"*.${domain}",	# FIXME: this is a temporary solution
+		],
+		#allow_duplicate_certs => true,	# redeploy without cert clean
+		repo => true,		# automatic repos
+		shorewall => false,
+		start => true,
+	}
+
+	class { '::puppet::deploy':
+		path => '/vagrant/puppet/',	# puppet folder is put here...
+		backup => false,		# don't use puppet to backup...
+	}
+}
+
+node default {	# all the other hosts
+
+	class { '::puppet::client':
+		#start => true,
+		start => false,			# useful for testing manually...
+	}
+
+	# add some puppet code here...
+}
+
