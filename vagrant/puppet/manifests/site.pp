ad5afa66be608b31055f4ad1338d082de0ba401d
James Shubin <james@shubin.ca>
2014-10-31 02:12:36 -0400

    Have omv act as a cheap ENC that is implemented in puppet.
    This makes it easy to select which puppet classes to include per host
    from within the omv vagrant.yaml file.
    
    This *is* the puppet integration you're looking for... (Or at least part
    of it!)

--- vagrant/puppet/manifests/site.pp
+++ vagrant/puppet/manifests/site.pp
@@ -6,6 +6,27 @@ class motd {
 	}
 }
 
+define omv_include() {	# this is a cheap enc in puppet and used by omv
+
+	# TODO: unfortunately we're passing data into puppet via the FACTER_
+	# environment variables that vagrant generates... this is limited and
+	# only allows strings without significantly more work (eg: pickle-ing)
+	# this is actually a limitation of puppet and vagrant, and not of omv!!
+	$params_str = getvar("::vagrant_puppet_classes_params_${name}")
+	if "${params_str}" != '' {
+		$params = split("${params_str}", ',')
+		$yaml = inline_template("<%= r = {}; @params.each {|x| r[x] = scope.lookupvar('::vagrant_puppet_classes_values_${name}_'+x)  }; r.to_yaml %>")
+		$parseyaml = parseyaml($yaml)
+		$hash = {
+			"${name}" => $parseyaml,
+		}
+		create_resources('class', $hash)
+
+	} else {
+		include "${name}"
+	}
+}
+
 # puppetmaster
 node puppet {
 
@@ -28,17 +49,30 @@ node puppet {
 		path => '/vagrant/puppet/',	# puppet folder is put here...
 		backup => false,		# don't use puppet to backup...
 	}
+
+	# dynamically include the list of classes for this host from omv enc...
+	if "${::vagrant_puppet_classes}" != '' {
+		$classes = split("${::vagrant_puppet_classes}", ',')
+		omv_include { $classes:	# loop!
+		}
+	}
 }
 
 node default {	# all the other hosts
 
 	include ::motd
 
+	# TODO: we could actually define the puppet classes with omv_include :)
 	class { '::puppet::client':
 		#start => true,
 		start => false,			# useful for testing manually...
 	}
 
-	# add some puppet code here...
+	# dynamically include the list of classes for this host from omv enc...
+	if "${::vagrant_puppet_classes}" != '' {
+		$classes = split("${::vagrant_puppet_classes}", ',')
+		omv_include { $classes:	# loop!
+		}
+	}
 }
 
