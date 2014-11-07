2ee7ec205b9339be6db9842cb13268bd14e7a0fb
Partha Aji <paji@redhat.com>
2014-11-07 18:16:11 -0500

    Prep 5

---
+++ vagrant/p4h/puppet/modules/p4h/manifests/new_class_prep5.pp
@@ -0,0 +1,29 @@
+# Puppet for Hackers course module by James
+# Copyright (C) 2013-2014+ James Shubin
+# Written by James Shubin <james@shubin.ca>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# README: this is a module built for use with: Oh My Vagrant!
+
+class p4h::new_class_prep5($file_name = "/root/README-Santa", $file_content = "Happy Holidays!!" ) {
+
+  file { $file_name:
+    content => "Y'all take care...
+
+${file_content}!\n",
+  }
+  include "::p4h::second_new_class_prep5"
+
+}
--- vagrant/p4h/puppet/modules/p4h/manifests/prep5.pp
+++ vagrant/p4h/puppet/modules/p4h/manifests/prep5.pp
@@ -41,7 +41,9 @@ Bonus:
 Happy hacking!\n",
 	}
 
-	# XXX: write your code here...
+  class {'::p4h::new_class_prep5':
+    file_name => "/tmp/life"
+  }
 
 }
 
