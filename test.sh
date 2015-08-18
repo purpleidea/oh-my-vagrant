28ec8c9ab3963be41ad8efab7b8cc29d62a92969
James Shubin <james@shubin.ca>
2015-08-18 14:23:39 -0400

    Add ruby-lint test to test case
    Unfortunately not all the checks are turned on at this time. If there is
    a ruby-lint hacker who can fix this, awesome! Until then, this is still
    better than nothing.

--- test.sh
+++ test.sh
@@ -4,3 +4,12 @@
 
 	# ensure there is no trailing whitespace or other whitespace errors
 	git diff-tree --check $(git hash-object -t tree /dev/null) HEAD
+
+	# ensure we pass basic ruby-lint checks
+	if which ruby-lint &>/dev/null; then	# don't fail if not installed
+		# TODO i would like to add on:
+		# -a undefined_methods -a undefined_variables
+		# but valid vagrant causes these to break at the moment.
+		# if this is fixed remove all the -a arguments for default use!
+		ruby-lint -a argument_amount -a loop_keywords -a pedantics -a shadowing_variables -a unused_variables -a useless_equality_checks vagrant/Vagrantfile
+	fi
