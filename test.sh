208538d288a22345cf564136921cb21bad534637
James Shubin <james@shubin.ca>
2015-09-02 12:46:06 -0400

    Add test case for AUTHORS file order
    This makes sure contributors put their name in the right order, while
    simultaneously making it fun for them to commit their own name :)
    
    Also, any time you add a test case is a good day!

--- test.sh
+++ test.sh
@@ -6,6 +6,10 @@ echo running test.sh
 # ensure there is no trailing whitespace or other whitespace errors
 git diff-tree --check $(git hash-object -t tree /dev/null) HEAD
 
+# ensure entries to authors file are sorted
+start=$(($(grep -n '^[[:space:]]*$' AUTHORS | awk -F ':' '{print $1}' | head -1) + 1))
+diff <(tail -n +$start AUTHORS | sort) <(tail -n +$start AUTHORS)
+
 # ensure we pass basic ruby-lint checks
 if which ruby-lint &>/dev/null; then	# don't fail if not installed
 	# TODO i would like to add on:
