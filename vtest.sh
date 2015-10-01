5fe4b23082f6dd00bf64c28d51d987b46511051a
James Shubin <james@shubin.ca>
2015-10-01 04:19:10 -0400

    Jtest2

--- vtest.sh
+++ vtest.sh
@@ -5,6 +5,8 @@ echo running vtest.sh
 
 #sudo dnf copr enable purpleidea/oh-my-vagrant
 #wget https://copr.fedoraproject.org/coprs/purpleidea/oh-my-vagrant/repo/epel-7/purpleidea-oh-my-vagrant-epel-7.repo
+whoami
+env
 wget https://copr.fedoraproject.org/coprs/purpleidea/oh-my-vagrant/repo/fedora-22/purpleidea-oh-my-vagrant-fedora-22.repo
-sudo cp -a purpleidea-oh-my-vagrant-fedora-22.repo /etc/yum.repos.d/
-sudo yum install -y oh-my-vagrant
+cp -a purpleidea-oh-my-vagrant-fedora-22.repo /etc/yum.repos.d/
+yum install -y oh-my-vagrant
